ecec86896496faf9eb77091db41076ee311e4e49
kishan128199 <kp132145@gmail.com>
2026-09-09 11:59:38 +0530

    Revert "Merge branch 'webhooks'"
    This reverts commit 16e4a25f6849b67e04e87c26c0a41502f8663fe5, reversing
    changes made to 54774f8f12c28d5ce61f29704f38911dd4eb0be3.

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
 const http = require('http');
 const { routeTasks } = require('./routes/tasks');
-const { notify } = require('./webhooks');
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  notify(req.url);
   if (req.url.startsWith('/tasks')) return routeTasks(req, res);
   res.writeHead(404).end('not found');
 });
--- api/webhooks.js
+++
@@ -1,6 +0,0 @@
-const subscribers = [];
-
-function subscribe(url) { subscribers.push(url); }
-function notify(event) { return subscribers.map((u) => ({ u, event })); }
-
-module.exports = { subscribe, notify };
